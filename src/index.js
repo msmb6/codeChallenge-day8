c55e830ec753aca2a16cd5e305065dfacd15eb69
msmb6 <88840662+msmb6@users.noreply.github.com>
2021-12-20 17:40:43 +0900

    update change parseInt innerHTML

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const randForm = document.getElementById("rand-form");
 const randRange = document.getElementById("rand-range");
 const userAnswer = document.getElementById("user-answer");
-const checkInput = randForm.querySelector("#answer-form h4:first-child");
-const checkAnswer = randForm.querySelector("#answer-form h4:last-child");
+const checkInput = randForm.querySelector("#answer-form p:first-child");
+const checkAnswer = randForm.querySelector("#answer-form p:last-child");
 
 function playRand(event) {
   event.preventDefault();
-  const machineAnswer = Math.floor(Math.random() * Number(randRange.value) + 1);
+  const machineAnswer = Math.ceil(
+    Math.random() * parseInt(randRange.value, 10)
+  );
   checkInput.innerText = `You chose: ${userAnswer.value}, the machine chose: ${machineAnswer}.`;
 
-  const answer = Number(userAnswer.value);
+  const answer = parseInt(userAnswer.value, 10);
   if (answer === machineAnswer) {
-    checkAnswer.innerText = "You win!";
+    checkAnswer.innerHTML = "<strong>You win!</strong>";
   } else {
-    checkAnswer.innerText = "You lose!";
+    checkAnswer.innerHTML = "<strong>You lose!</strong>";
   }
 }
 randForm.addEventListener("submit", playRand);
